78773978953aefd03aa9f4c05f7d5e98c4ed1887
Madina <dimasempaj@gmail.com>
2023-02-14 23:01:42 +0500

    commit 2.1

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ let questionInput = document.querySelector('.question-input');
 let send = document.querySelector('.sending-button');
 let messageBlock = document.querySelector('.message-block');
 
-send.addEventListener('click', function () {
+questionInput.addEventListener('keydown', function (event){
+    if(event.code === 'Enter') {
+        submitQuestion();
+        questionInput.value = '';
+    }
+})
+
+function submitQuestion() {
     let messageContainer = document.createElement('div');
     messageBlock.append(messageContainer);
     messageContainer.classList.add('message');
     messageContainer.classList.add('question');
     messageContainer.innerHTML = questionInput.value;
-    if(questionInput.value.trim()){
-        questionInput.value = ''
-    }
+    questionInput.value = '';
 
     fetch('https://chat-gpt-example.vercel.app/makeRequest', {
         method: 'POST',
@@ -25,17 +30,19 @@ send.addEventListener('click', function () {
             return response.json()
         })
         .then(function (res) {
-            console.log(res);
             let answerContainer = document.createElement('div');
-            answerContainer.classList.add('message');
             let answerDiv = document.createElement('pre')
+            answerContainer.classList.add('message');
             answerDiv.classList.add('answer');
-
             answerContainer.append(answerDiv);
             messageBlock.append(answerContainer);
 
             answerDiv.innerHTML = res.answer.trim();
-            questionInput.value = '';
         })
+}
+
+send.addEventListener('click', function () {
+    submitQuestion();
+    questionInput.value = ''
 });
 
